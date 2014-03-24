c555a81adefcfb441e11dae72e644ef6906c5b7d
D'Amico Giuseppe <damicogiuseppe77@gmail.com>
2014-03-24 17:59:43 +0100

    addAddress test fully functional

--- test/e2e/addAddress_spec.js
+++ test/e2e/addAddress_spec.js
@@ -33,8 +33,19 @@ it("should add an item to the address list with class 'nuovo'",function(){
             expect(arr.length).toEqual(initial_length+1);
             var inserted_item = element(arr[arr.length-1]) //get the last element, the just inserted one
             //console.log(inserted_item);
-            var lastRow = element(by.repeater('a in customer.LIVES_IN').last);
-            expect(lastRow).toHaveClass('nuovo');
+            var lastRow = arr[2];
+            var attributes = '';
+            //console.log(
+                arr[2].getAttribute('className').then(function(o){
+                                                                           // console.log('then');
+                                                                            //console.log(o)
+                                                                            attributes = o;
+                                                                            //console.log(attributes);
+                                                                            var pos = attributes.indexOf('nuovo'); // if != -1 nuovo is in the attribute's list
+                                                                            //console.log(attributes.indexOf('nuovo'));
+                                                                            expect(pos!=-1).toBe(true);
+                                                                         });
+            
         })
 });
     
